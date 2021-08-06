17b99f2b4ee556cc7cb857c0f4eb82fdda74c954
Sagar Shelar <sagarshelar49@gmail.com>
2021-08-06 15:14:58 +0530

    initail commit

---
+++ src/test/java/com/sp/trading/TradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
